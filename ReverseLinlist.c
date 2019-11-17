6aebaef6c10d86792488d801090526a95372ee4b
LikeaShadiao <48277292+LikeaShadiao@users.noreply.github.com>
2019-11-17 23:40:47 +0800

    Update ReverseLinlist.c
    make the code whole

--- ReverseLinlist.c
+++ ReverseLinlist.c
@@ -1,3 +1,4 @@
+//这是List的数据类型
 typedef struct Node *PtrToNode;
 struct Node {
     ElementType Data; /* 存储结点数据 */
@@ -5,6 +6,7 @@ struct Node {
 };
 typedef PtrToNode List; /* 定义单链表类型 */
 
+//这是实现逆置的函数
 List Reverse(List L)
 {
     List Temp,Prev;
@@ -18,3 +20,90 @@ List Reverse(List L)
     }
     return Prev;
 }
+
+//这是完整代码，亲测有效
+#include<stdio.h>  
+#include<stdlib.h>  
+typedef int ElementType;  
+typedef struct Node *PtrToNode;  
+struct Node {  
+    ElementType Data;  
+    PtrToNode Next;  
+};  
+typedef PtrToNode List;  
+List Read();  
+void Print(List L);  
+List Reverse(List L);  
+  
+int main()  
+{  
+    List L1, L2;  
+    L1 = Read();  
+    L2 = Reverse(L1);  
+    Print(L1);  
+    Print(L2);  
+  
+    return 0;  
+}  
+  
+/* 建立链表 */  
+List Read()  
+{  
+    List head = NULL;  
+    List current;  
+    List prev = NULL;  
+    int len = 0;  
+    scanf("%d", &len);  
+    if (len == 0)   return NULL;  
+    
+    while (len--)   
+    {  
+        current = (List)malloc(sizeof(struct Node));  
+        if (head == NULL)  
+            head = current;  
+        else  
+        prev->Next = current;  
+        current->Next = NULL;  
+        scanf("%d", &current->Data);  
+        prev = current; 
+    }  
+    return head;  
+} 
+void Print(List L)  
+{  
+    List p = L;  
+    List s = L;  
+    List temp;  
+    if (p == NULL)  
+        printf("NULL");  
+    else  
+        printf("\n");  
+    while (p!=NULL) {  
+        printf("%d ", p->Data);  
+        p = p->Next;  
+    }  
+}  
+  
+List Reverse( List L )  
+{  
+    List Temp, Prev;  
+    Prev = NULL;  
+    while(L)  
+    {  
+        Temp = L->Next;  
+        L->Next = Prev;  
+        Prev = L;  
+        L = Temp;  
+    }  
+    return Prev;  
+}  
+//输入输出大致如下
+/*
+输入
+5
+1 2 3 4 5
+
+输出
+1
+5 4 3 2 1
+*/
