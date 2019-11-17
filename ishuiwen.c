3eefc3dfe62830125554d245319cad4b2ded59b0
LikeaShadiao <48277292+LikeaShadiao@users.noreply.github.com>
2019-11-17 18:42:21 +0800

    save a code to judeg huiwen number
    first save a question in net

---
+++ ishuiwen.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int a,b=0,c,d=0;
+	scanf("%d",&a);
+	c=a;
+	while(a)
+	{
+		b=b*10+a%10;
+		d+=a%10;
+		a/=10;
+	}
+	if(b==c)
+	printf("%d",d);
+	else
+	printf("no");
+	
+}
